42d65d433fa79bc147d75a791f8d8e93e87bea5c
magdalenaz123 <56075253+magdalenaz123@users.noreply.github.com>
2019-10-02 15:00:11 +0200

    Update 1_zadatak.py

--- vjezbe/1_zadatak.py
+++ vjezbe/1_zadatak.py
@@ -1,3 +1,13 @@
 
 #ZADATCI(riješite, dokumentirajte I zatim pročitajte docstring): 
 #Kreirajte funkciju koja računa zbroj svih znamenaka nekog cijelog broja 
+
+def zbroj(n)
+   '''funkcija koja računa zbroj znamenaka cijelog broja'''
+   L=list(str(n))
+   b=0
+   for i in L:
+       b+=int(i)
+    return b
+    x=int(input('upisi cijeli broj:'))
+print(zbroj(x)) 
